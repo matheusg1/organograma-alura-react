0beff9b4b307e0c3827c8f3cea404fd2bbbe533d
matheus.silva <tecmatheusg@hotmail.com>
2023-03-20 14:59:58 -0300

    useStates

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import TextInput from "../TextInput";
 import "./Form.css"
 import DropdownList from "../DropdownList";
@@ -13,19 +14,35 @@ const Form = () => {
         "Inovação e gestão"
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
     const aoSalvar = (e) => {
         e.preventDefault();
-        console.log("Form submetido");
-        console.log(e.Nome);
+        console.log("Form submetido " + nome + cargo + imagem);
+
     }
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextInput label="Nome" placeholder="Digite seu nome"></TextInput>
-                <TextInput label="Cargo" placeholder="Digite seu cargo"></TextInput>
-                <TextInput label="Imagem" placeholder="Digite o endereço da imagem" />
-                <DropdownList label="Time" itens={times}/>
+                <TextInput
+                    label="Nome"
+                    aoAlterar={valor => setNome(valor)}
+                    placeholder="Digite seu nome"></TextInput>
+
+                <TextInput
+                    label="Cargo"
+                    aoAlterar={valor => setNome(valor)}
+                    placeholder="Digite seu cargo"></TextInput>
+
+                <TextInput
+                    label="Imagem"
+                    aoAlterar={valor => setNome(valor)}
+                    placeholder="Digite o endereço da imagem" />
+                    
+                <DropdownList label="Time" itens={times} />
                 <Button>Criar card</Button>
             </form>
         </section>
